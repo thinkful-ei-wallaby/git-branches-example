9f41389e9634f663415778f001ed7d1e2adcef79
Terra <terra.taylor@gmail.com>
2021-02-01 11:41:51 -0700

    changed random facts

--- generator.js
+++ generator.js
@@ -1,5 +1,5 @@
 function generateRandomFact(){
-    let randomFacts = [`I'm 5'11"`, `I was the best speller in my middle school`, ' I was a martial artist 15+ years ago',` I can fly an airplane`, `been to Nepal
+    let randomFacts = [`I'm 5'11"`,`Tuskegee Airman Grandpa`, `I was the best speller in my middle school`, ' I was a martial artist 15+ years ago',` I can fly an airplane`, `been to Nepal
     `, `billingual`, ` I used to be a gymnast`];
     let randomIndex =Math.floor((Math.random() * randomFacts.length));
     console.log(randomFacts[randomIndex]);   
